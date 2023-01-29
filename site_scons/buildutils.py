speth/cantera

commit 97fc050bdec609d0cd3a0af863b41c59aa5a9dce

Rewrite which function to use pathlib

site_scons/buildutils.py
@@ -537,25 +537,15 @@ def mglob(env, subdir, *args):
     return matches
 
 
-def which(program):
-    """ Replicates the functionality of the 'which' shell command """
-    def is_exe(fpath):
-        for ext in ('', '.exe', '.bat'):
-            if os.path.exists(fpath + ext):
-                return os.access(fpath + ext, os.X_OK)
-        return False
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
+def which(program: str) -> bool:
+    """Replicates the functionality of the 'which' shell command."""
+    for ext in ("", ".exe", ".bat"):
+        fpath = Path(program + ext)
         for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
+            exe_file = Path(path).joinpath(fpath)
+            if exe_file.exists() and os.access(exe_file, os.X_OK):
+                return True
+    return False
 
 optionWrapper = textwrap.TextWrapper(initial_indent='    ',
                                    subsequent_indent='    ',

SConstruct
@@ -1162,7 +1162,7 @@ program main
    write(*,'(a)') 'Hello, world!'
 end program main
     '''
-    if which(compiler) is not None:
+    if which(compiler):
         env['F77'] = env['F90'] = env['F95'] = env['F03'] = env['FORTRAN'] = compiler
         success, output = conf.TryRun(hello_world, '.f90')
         if success and 'Hello, world!' in output:

commit 174278195e740ad82ac98e068cde48e788f411a5

[SCons] Restore functionality of buildutils.which
Broken in 97fc050bde

site_scons/buildutils.py
@@ -1139,15 +1139,15 @@ def multi_glob(env: "SCEnvironment", subdir: str, *args: str):
     return matches
 
 
-def which(program: str) -> bool:
+def which(program: str) -> "Optional[str]":
     """Replicates the functionality of the 'which' shell command."""
     for ext in ("", ".exe", ".bat"):
         fpath = Path(program + ext)
         for path in os.environ["PATH"].split(os.pathsep):
             exe_file = Path(path).joinpath(fpath)
             if exe_file.exists() and os.access(exe_file, os.X_OK):
-                return True
-    return False
+                return str(exe_file)
+    return None
 
 
 def listify(value: "Union[str, Iterable]") -> "List[str]":